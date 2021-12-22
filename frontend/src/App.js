72032a898d654361558eeddc43b6bf07339cb707
princekr96 <princekr700@gmail.com>
2021-12-22 23:13:30 +0530

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
-import { Fragment } from 'react';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
 
 function App() {
   return (
-    <Fragment>
-      <NavBar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
