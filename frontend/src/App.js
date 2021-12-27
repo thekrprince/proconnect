186af9eb53cd8061a8a3d99d21cb410cc1980103
princekr96 <princekr700@gmail.com>
2021-12-28 01:06:57 +0530

    Set and Remove alert implemented using Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,32 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-        <section className="container">
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <NavBar />
           <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Landing />} />
           </Routes>
-        </section>
-      </Fragment>
-    </Router>
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
